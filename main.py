8a2cf2d02a180f31427b0c235e4863e0d472a246
peteris.troksa <peteris.troksa@vraa.gov.lv>
2024-06-24 17:01:29 +0300

    1.diena b)Print versijas final

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 print("Hy" + "Kā iet?")
 print("Hy " + "Kā iet?")
 print("Hy" + " " + "Kā iet?")
+
+kad ievievieto vērtības
+
+print("Tevi sauc (v1)" + input("Vārds ir"))
+
+vards = input("Kā tevi sauc\n")
+print("Tavs vārds ir" + vards)
+
+num1 = 1
+num2 = 2
+
+print(num1 + num2)
+rezultats = int(num1 + num2)
+print(rezultats)
+
+inpu = "Biete's"
+print(len(inpu))
+
+name = input("Kā tevi sauc? \n")
+
+print("Tavas grupas nosaukums ir " + name + " grupa" )
