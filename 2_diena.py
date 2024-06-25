4992e297be2cdfb850bdd91db26ff5c83e13e643
peteris.troksa <peteris.troksa@vraa.gov.lv>
2024-06-25 17:28:17 +0300

    3.diena  gatavs kalkulators

--- 2_diena.py
+++ 2_diena.py
@@ -6,13 +6,41 @@
 # print(jaunais_garums)
 # print("Tavs vārds ir " + jaunais_garums + " zīmes")
 
-a = 123
-print(type(a))
-b = str(a)
-print(type(b))
-
-c = "100.5"
-print(type(c))
-d = float(c)
-print(70 + d)
+# a = 123
+# print(type(a))
+# b = str(a) #var izmantot aŗi float(a) str(a) int(a)
+# print(type(b))
+#
+# c = "100.5"
+# print(type(c))
+# d = float(c)
+# print(70 + d)
 # = 170.5
+
+#_____________________________________________
+# print(8/3)
+# print(round(8/3))
+# print(round(8/3, 4))
+# print(8//3) #uzreiz ar divām // veselus dalījumus dod
+# print(type(8//3))
+
+# result = 4/0.7
+# result /= 2
+# print(result)
+
+# score = 0
+# print(score)
+# score += 1
+# print(score)
+#_________________________________Tip Calcuslator______________
+
+print("Welcom the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12 ar 15? "))
+pers = int(input("How manu people to split the bill? "))
+# print(type(bill))
+# print(type(tip))
+amount1 = float((tip/100) * bill)
+amount2 = round((amount1 + bill)/pers, 2)
+print(f"Each person should pay: ${amount2}")
+
