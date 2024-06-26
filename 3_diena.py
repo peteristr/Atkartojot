123abcfa393ec3db4fdafeaa1d6aeff8cabac1bf
peteris.troksa <peteris.troksa@vraa.gov.lv>
2024-06-26 16:10:49 +0300

    3.diena  if elif rolercoster/leap year/picas pasūtīšana

--- 3_diena.py
+++ 3_diena.py
@@ -1,5 +1,5 @@
-print("Welcome to the rollercoastrer!")
-height = int(input("What is your heigt in cm? "))
+# print("Welcome to the rollercoastrer!")
+# height = int(input("What is your heigt in cm? "))
 
 # bill = 0
 # if height >= 120: #tikai ja garums ir virs 120 cm citādi else par mazu
@@ -104,3 +104,59 @@ height = int(input("What is your heigt in cm? "))
 #     kaut
 #     kas
 #_________________________________________Picas pasūtīšana________________________
+# Instructions
+# Congratulations, you've got a job at Python Pizza!
+# Your first job is to build an automatic pizza order program.
+# Based on a user's order, work out their final bill.
+#
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+#
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+# Add extra cheese for any size pizza (Y or N): +$1
+#
+# Example Input/testēšanas scenārijs
+# L
+# Y
+# N
+# Example Output
+# Thank you for choosing Python Pizza Deliveries!
+# Your final bill is: $28.
+
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L :  ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+
+
+bill = 0
+if size == "S":
+    bill += 15
+    if add_pepperoni == "Y":
+        bill += 2
+    if extra_cheese == "Y":
+        bill += 1
+        print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
+    else:
+        print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
+elif size == "M":
+    bill +=20
+    if add_pepperoni == "Y":
+        bill +=3
+    if extra_cheese == "Y":
+        bill += 1
+        print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
+    else:
+        print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
+elif size == "L":
+    bill +=25
+    if add_pepperoni == "Y":
+        bill +=3
+    if extra_cheese == "Y":
+        bill += 1
+        print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
+    else:
+        print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
