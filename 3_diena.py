8936b4e64eb031448ca75bafcf70613550f97835
peteris.troksa <peteris.troksa@vraa.gov.lv>
2024-06-26 09:22:15 +0300

    3.diena  if elif rolercoster

--- 3_diena.py
+++ 3_diena.py
@@ -1,12 +1,17 @@
 print("Welcome to the rollercoastrer!")
 height = int(input("What is your heigt in cm? "))
 
-if height >=120:
-    print("Garāks par nepieciešamo.")
-    vecums = int(input("Cik gadu"))
-    if vecums >=7 :
+if height >= 120: #tikai ja garums ir virs 120 cm citādi else par mazu
+    print("Garāks par nepieciešamo. ") # ja garāks, tad prasa vecumu
+    vecums = int(input("Cik gadi? "))
+    if vecums >18 : #
+        print("Jāmaksā ir 12$")
+    elif vecums >=12 or vecums == 18:
         print("Jāmaksā ir 7$")
-    else:
-        print("Par brīvu, lūdzu")
+    elif vecums < 12:
+        print("Tev kā bērnam jāmaksā 5$")
 else:
     print("Sorry tu esi par mazu!")
+
+# ja kāds ir zem 18 vai vienāds tad $7
+# ja kāds ir virs 18 tad 12$
