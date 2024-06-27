2b7339fbbfb3c14283b185dc9c7f94450ed107d5
peteris.troksa <peteris.troksa@vraa.gov.lv>
2024-06-27 16:14:29 +0300

    4.diena  heads and tail ar todo

---
+++ 4.diena.py
@@ -0,0 +1,51 @@
+import random
+
+# number = random.randint(1,100)
+# print(number)
+#
+# random_float = random.random()
+# print(random_float)
+#
+# #random float can bi from 0.0000-0.999999
+# random_float = random.random()
+# print(random_float)
+#
+# love_score = random.randint(1, 100)
+# print(f"Your love score ir {love_score}")
+
+# You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+#
+# Important, the first letter should be capitalised and spelt exactly like in the example e.g. "Heads", not "heads".
+#
+# There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number,
+# either 0 or 1. Then use that number to print out "Heads" or "Tails".
+#
+# e.g. 1 means Heads 0 means Tails
+#
+# Example Output
+# Heads
+# or
+#
+# Tails
+
+# Heads = 0
+# Tails = 1
+#
+# random_h_t = random.randint(0,1)
+# print(random_h_t)
+#
+# game = int(input("Heads or tails? Type 0 or 1 "))
+# # game_to_int = int(game)
+# print(type(game))
+# if game == 0 and game == random_h_t or game == 1 and game == random_h_t:
+#     print(f"You win {random_h_t}")
+# elif game == 0 and game != random_h_t:
+#     print(f"You lose {random_h_t}")
+# # elif game == 1 and game == random_h_t:
+# #     print(f"You win {random_h_t}")
+# else:
+#     print(f"You lose {random_h_t}")
+
+#______________________________________________________
+#TODO - 1 - Ceturtā diena head or tails (if, ==, if and or, random)
+
