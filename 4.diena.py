7306c3bc8787a52248293feb9eb2c140b308a0ef
peteris.troksa <peteris.troksa@vraa.gov.lv>
2024-06-27 16:44:33 +0300

    4.diena  heads and tail/random payer ar todo

--- 4.diena.py
+++ 4.diena.py
@@ -49,3 +49,52 @@ import random
 #______________________________________________________
 #TODO - 1 - Ceturtā diena head or tails (if, ==, if and or, random)
 
+#Lists
+# cipari = [1,2,3]
+# print(cipari[-1]) #izpintē pēdējo no sarkasta
+#
+# cipari[2] = 4 #aizvieto pēdējo jeb trijnieku (2 jo 0,1,2) ar jaunu skaitli
+# print(cipari)
+#
+# cipari.append(5) #ievieto ar .apend funkciju saraksta beigās
+# print(cipari)
+#
+# cipari.extend([0,8,99]) #extend izmanto lai pieiktu sarakstu galā
+# print(cipari)
+
+#______________________________________________________
+
+# You are going to write a program that will select a random name from a list of names.
+# The person selected will have to pay for everybody's food bill.
+# Important: You are not allowed to use the choice() function.
+#
+# NOTE: In this exercise, you are working collaboratively with another programmer.
+# They already dealt with input() and writing the code needed to get hold of the names in the
+# input area, so you don't need to worry about that.
+#
+# The other programmer has written the code to separate the names in the input area into individual names and
+# puts them inside a List called names. For their code to work correctly, you must enter all the names in the
+# input area followed by comma then space. e.g. name, name, name
+#
+# You can try printing names to see what it looks like (but remember to remove that code when you submit the assignment).
+# Assume that names works like this:
+#
+# input area: x, y, z,
+# names = ["x", "y", "z"]
+# Example Input
+# Angela, Ben, Jenny, Michael, Chloe
+# Note: notice that there is a space between the comma and the next name.
+#
+# Example Output
+# Michael is going to buy the meal today!
+# Hints
+# You might need the help of the len() function. https://stackoverflow.com/questions/1712227/how-do-i-get-the-number-of-elements-in-a-list
+#
+# Remember that Lists start at index 0!
+
+# #
+# payers_list = ['Angela', 'Ben', 'Jenny', 'Michael', 'Chloe']
+# random_payer = random.choice(payers_list)
+# print(f"{random_payer} is going to buy the meal today! ")
+# TODO - 2 - Ceturtā diena random bill player (random.choose from list) arī lietas par to kā papilidnāt list [] aizvietot, parādīt
+#______________________________________________________
